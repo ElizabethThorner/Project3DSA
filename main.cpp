9958c94bec8e0485490bfdd3bd4ad7843f683649
ZhenXW77 <73552471+ZhenXW77@users.noreply.github.com>
2021-12-06 06:11:13 -0500

    Zhen's Dijkstra implementation
    100,000 nodes with 3 edges took 8:12 to run.

--- main.cpp
+++ main.cpp
@@ -14,19 +14,23 @@
 #include <unordered_set>
 #include <unordered_map>
 
+#include "Dijkstra.h"
+
 using namespace std::chrono;
 using namespace std;
 typedef high_resolution_clock Clock;
 
-int main()
+vector<int> Dijkstra(map<int, set<pair<int, int>>>& _graph, int _numNodes, int _source);
+
+int main() 
 {
-    //graph variables
+    //graph varibles
     int numberNode = -1;
     int maxEdgePerNode = -1;                                     //minimum of 2 edges (2 edges will just be a straight path)
 
     while (numberNode < 2)
     {
-        cout << "Enter the number of nodes: ";
+        cout << "Enter the number of node: ";
         cin >> numberNode;
     }
 
@@ -52,103 +56,122 @@ int main()
     //add all the address and edges to the graph
     for (int i = 1; i <= numberNode; i++)
     {
-        //from itself to 2 other possible neighbor within (itself + 2) to (itself + 12) (ex: address 3 can have numEdgePerNode edges to address 5 - 15)
+        //from itself to 2 other possble neighbor within (itself + 2) to (itself + 12) (ex: address 3 can have numEdgePerNode edges to address 5 - 15)
         set<int> nearbyNeighbor;
         while ((nearbyNeighbor.size() != maxEdgePerNode - 2) && nearbyNeighbor.size() != numberNode - i - 1 && i != numberNode) //2 edges is reserved for straight path
-            {
+        {
             random = i + neighborRange(rd);
 
-            if (random <= numberNode)                            // neighbor edge can't be larger than number of nodes that exist
-                {
+            if (random <= numberNode)                            // neighbor edge cant be larger then number of nodes that exist
+            {
                 nearbyNeighbor.emplace(random);
-                }
             }
+        }
 
         //add edges from current address to nearby neighbor
         for (auto j = nearbyNeighbor.begin(); j != nearbyNeighbor.end(); j++)
         {
             if (edgeCount[i] < maxEdgePerNode - 1 && edgeCount[*j] < maxEdgePerNode - 2)    //save 1 edge for self and 2 edge for nearby Neighbor
-                {
+            {
                 random = distanceRange(rd);
                 graph[i].emplace(make_pair(*j, random));
                 graph[*j].emplace(make_pair(i, random));
                 edgeCount[i] += 1;
                 edgeCount[*j] += 1;
-                }
+            }
         }
 
+
         random = distanceRange(rd);
-        graph[i].emplace(make_pair(i + 1, random));             //edge for the next neighbor (from, to)
+        graph[i].emplace(make_pair(i + 1, random));             //edge for the next neighbor (from, to) 
         graph[i + 1].emplace(make_pair(i, random));             //                           (to, from)
         edgeCount[i] += 1;                                      //increment edge tracker
         edgeCount[i + 1] += 1;
+
     }
 
-    graph.erase(numberNode + 1);                                //delete the one excess node from the graph
-    graph[numberNode].erase(make_pair(numberNode + 1, random)); //delete the unreachable node from the last address
-
+    graph.erase(numberNode + 1);                                //delete the one excess ndoe from the graph
+    graph[numberNode].erase(make_pair(numberNode + 1, random)); //delete the unreachable ndoe from the last address
+ 
     //cout << "map size: " << graph.size() << endl;
     //print the entire map
-    for (auto i = graph.begin(); i != graph.end(); i++)
+    /*for (auto i = graph.begin(); i != graph.end(); i++)
     {
         cout << "index: " << i->first << endl;
         cout << "edges: ";
 
         for (auto j = i->second.begin(); j != i->second.end(); j++)
         {
-            cout << "(" << j->first << ", " << j->second << "); ";
+            cout << "(" << j->first << ", " << j->second<< "); ";
         }
 
         cout << endl << endl;
+    }*/
+
+    //generate random amount of delivery address
+    /*static uniform_int_distribution<int> deliveryRange(2, numberNode);     // random
+    int numDeliveryAddress = 2;
+    set<int> deliveryList;
+
+    while (deliveryList.size() != numDeliveryAddress)
+    {
+        deliveryList.emplace(deliveryRange(rd));
     }
 
-
-    cout << "Generating an efficient path through Djikstra's algorithm..." << endl;
-
-    set<int> addressesAdded;
-    const int infinity = 2147483647;
-    vector<int> dijkstraPath;
-    for (auto iter = graph.begin(); iter != graph.end(); iter++) {
-        set<int> processed;
-        vector<int> dist(graph.size(), infinity);
-        dist[iter->first] = 0;
-        addressesAdded.insert(iter->first);
-
-        while (processed.size() < graph.size()) {
-            int v = 0;
-            for (auto iter2 = graph.begin(); iter2 != graph.end(); iter2++) {
-                if (processed.count(iter2->first) == 0) {
-                    v = iter2->first;
-                    break;
-                }
-            }
-            for (auto iter2 = graph.begin(); iter2 != graph.end(); iter2++) {
-                if ((processed.count(iter2->first) == 0) && (dist[iter2->first] < dist[v])) {
-                    v = iter2->first;
-                }
-            }
-            processed.insert(v);
-            for (auto j = graph[v].begin(); j != graph[v].end(); j++) {
-                if (processed.count(j->first) == 0) {
-                    if (dist[j->first] > dist[v] + j->second) {
-                        dist[j->first] = dist[v] + j->second;
-                    }
-                }
-            }
-        }
-        int min = infinity;
-        int minIndex = 0;
-        for (int i = 0; i < dist.size(); i++) {
-            if (addressesAdded.count(i) == 0) {
-                if (dist[i] < min) {
-                    min = dist[i];
-                    minIndex = i;
-                }
-            }
-        }
-        dijkstraPath.push_back(minIndex);
+    for (auto i = deliveryList.begin(); i != deliveryList.end(); i++)
+    {
+        cout << *i << ", ";
     }
+    cout << endl;*/
 
+    Dijkstra(graph, numberNode, 1);
 
+    cout << "Dijkstra finished";
     return 0;
+}
+
+//return the distance from source to each node
+vector<int> Dijkstra(map<int, set<pair<int, int>>>& _graph, int _numNodes, int _source)
+{
+    vector<int> visited (_numNodes + 1, 0);                     // keep track which nodes is visited
+    vector<int> distance(_numNodes + 1, 300000000);              // track the distance from the source node 
+
+    int minIndex = _source;
+    distance[_source] = 0;
+
+    for (int i = 0; i < _numNodes; i++)
+    {
+        int min = 300000000;
+
+        //find the smallest distance thats not been to
+        for (int k = 0; k < distance.size(); k++)
+        {
+            if (distance[k] < min && visited[k] == 0)
+            {
+                min = distance[k];
+                minIndex = k;
+            }
+        }
+
+        //do calculate to its childrens
+        for (auto j = _graph[minIndex].begin(); j != _graph[minIndex].end(); j++)
+        {
+            //if the new distance is less then update
+            if (distance[minIndex] + j->second < distance[j->first])
+            {
+                distance[j->first] = distance[minIndex] + j->second;
+            }
+        }
+
+        //finished visiting this node
+        visited[minIndex] = 1; //set to true 
+    }
+
+    //test print
+    /*for (int i = 0; i < distance.size(); i++)
+    {
+        cout << "Distance to " << i << ": " << distance[i] << endl;
+    }*/
+
+    return distance;
 }
